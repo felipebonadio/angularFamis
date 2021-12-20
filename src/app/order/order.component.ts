9aef2725e0e3a9d697fca4311a0e69233ee0c3e9
felipebonadio <febonadio@gmail.com>
2021-12-20 16:13:11 -0300

    Fixed css errors

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -5,24 +5,18 @@ import { AdminService } from '../service/admin.service';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
+  restaurant: Restaurant;
 
-  restaurant : Restaurant;
-  consumer : number;
-
-  
   constructor(private adminService: AdminService) {
     this.restaurant = {} as Restaurant;
-    this.consumer = this.restaurant.consumer;
-   }
-   
+  }
 
   ngOnInit(): void {
     this.adminService
       .getRestaurants()
       .subscribe((restaurant) => (this.restaurant = restaurant[0]));
   }
-
 }
